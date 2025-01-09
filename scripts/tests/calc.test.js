3773bee3f199c2ad9ca71712603305eb2e381084
Anna Teilane <kastekastee@gmail.com>
2025-01-09 16:43:47 +0000

    further tests

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -4,7 +4,10 @@ describe("Calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(22, 20)).toBe(42);
-        })
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
     });
     describe("Subtraction function", () => {
         
